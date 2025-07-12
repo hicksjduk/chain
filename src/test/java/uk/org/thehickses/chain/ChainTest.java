18f6b5c2cd693eb4904d3dbf5978b370ff0821cf
Jeremy Hicks <jeremy@thehickses.org.uk>
2025-07-12 11:02:47 +0100

    Update versions

--- src/test/java/uk/org/thehickses/chain/ChainTest.java
+++ src/test/java/uk/org/thehickses/chain/ChainTest.java
@@ -7,7 +7,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ChainTest
 {
