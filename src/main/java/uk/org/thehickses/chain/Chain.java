dacc33ff0605ca9bd714c2e515729c1a33787d11
Jeremy Hicks <jeremy@thehickses.org.uk>
2025-07-16 13:59:33 +0100

    Tidy

--- src/main/java/uk/org/thehickses/chain/Chain.java
+++ src/main/java/uk/org/thehickses/chain/Chain.java
@@ -329,7 +329,6 @@ public class Chain
                     try
                     {
                         run();
-                        ;
                     }
                     catch (NullPointerException ex)
                     {}
